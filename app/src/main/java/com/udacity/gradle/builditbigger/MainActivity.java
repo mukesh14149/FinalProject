3cffca3cc65713ad1a310e125e0250f925a4353b
Mukesh Gupta <mukesh14149@iiitd.ac.in>
2016-12-08 00:50:50 +0530

    Step 0,1,2 completed

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -0,0 +1,50 @@
+package com.udacity.gradle.builditbigger;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.Joke;
+import com.example.showjoke.PresentJoke;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void tellJoke(View view) {
+        Joke myJoker = new Joke();
+        startActivity(PresentJoke.launchIntent(this, myJoker.getJoke()));
+    }
+
+
+}
---
+++ jokelib/src/main/java/com/example/Joke.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Joke {
+    public Joke() {
+
+    }
+
+    public String getJoke() {
+        return "Very funny joke!";
+    }
+}
